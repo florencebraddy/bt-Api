4826e04d18289dbc1fa5a7fd6c37a43f402541eb
Florence Braddy <florencebraddy@hotmail.com>
2020-08-01 14:27:10 -0400

    completed dateVerification and repoVerification

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const axios = require("axios");
 //       response.status == 200
 //     ) {
 //       console.log("SUCCESS");
+//       //check on this
 //       console.log(origResponse.id);
 //     }
 //   } catch (origResponse) {
@@ -21,15 +22,56 @@ const axios = require("axios");
 // }
 // getOutputData();
 
-async function verification() {
-  try {
-    const response = await axios.get("http://api.github.com/orgs/boomtownroi");
-    var origResponse = response.data;
-    console.log(response.data);
-    if (origResponse.updated_at > origResponse.created_at)
-      console.log("updated_at is later than created_at date");
-  } catch (error) {
-    console.log(error);
-  }
-}
-verification();
+// async function dateVerification() {
+//   try {
+//     const response = await axios.get("http://api.github.com/orgs/boomtownroi");
+//     var origResponse = response.data;
+//     console.log(response.data);
+//     if (origResponse.updated_at > origResponse.created_at)
+//       console.log("updated_at is later than created_at date");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// dateVerification();
+
+// async function repoVerification() {
+//   try {
+//     const response = await axios.get("http://api.github.com/orgs/boomtownroi");
+//     var origResponse = response.data;
+//     var publicRepoCount = origResponse.public_repos;
+//     // console.log("Public Repo count:", publicRepoCount);
+
+//     var repoURL = origResponse.repos_url;
+//     // console.log(repoURL);
+
+//     const repoResponse = await axios.get(
+//       "https://api.github.com/orgs/BoomTownROI/repos"
+//     );
+//     // console.log(repoResponse);
+
+//     var repoArray = repoResponse.data;
+
+//     var repoCount = Object.keys(repoArray).length;
+//     // console.log(repoCount);
+
+//     const secondPageResponse = await axios.get(
+//       "https://api.github.com/orgs/BoomTownROI/repos?page=2"
+//     );
+
+//     var repoArray2 = secondPageResponse.data;
+//     // console.log(repoArray2);
+
+//     var secondRepoCount = Object.keys(repoArray2).length;
+//     // console.log(secondRepoCount);
+//     var totalRepoCount = repoCount + secondRepoCount;
+//     // console.log(totalRepoCount);
+
+//     if (publicRepoCount == totalRepoCount) {
+//       // console.log("Counts Match");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// repoVerification();
