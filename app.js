40fdbf0479c694d97cc37331c05bd48964af1495
Florence Braddy <florencebraddy@hotmail.com>
2020-07-31 23:05:18 -0400

    updated_at and created_at verification

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-const express = require("express");
 const axios = require("axios");
 
-async function getBoomTownInfo() {
+// async function getOutputData() {
+//   try {
+//     const response = await axios.get("http://api.github.com/orgs/boomtownroi");
+//     var origResponse = response.data;
+//     var valueArray = Object.values(origResponse);
+//     var singleValue = valueArray[5];
+//     var singleValToString = JSON.stringify(singleValue);
+//     console.log(singleValToString);
+//     if (
+//       singleValToString.includes("api.github.com/orgs/BoomTownROI") &&
+//       response.status == 200
+//     ) {
+//       console.log("SUCCESS");
+//       console.log(origResponse.id);
+//     }
+//   } catch (origResponse) {
+//     console.log(origResponse);
+//   }
+// }
+// getOutputData();
+
+async function verification() {
   try {
     const response = await axios.get("http://api.github.com/orgs/boomtownroi");
     var origResponse = response.data;
-    var valueArray = Object.values(origResponse);
-    var singleValue = valueArray[5];
-    var singleValToString = JSON.stringify(singleValue);
-    console.log(singleValToString);
-    if (
-      singleValToString.includes("api.github.com/orgs/BoomTownROI") &&
-      response.status == 200
-    ) {
-      console.log("SUCCESS");
-      console.log(origResponse.id);
-    } else {
-      console.log(origResponse);
-    }
+    console.log(response.data);
+    if (origResponse.updated_at > origResponse.created_at)
+      console.log("updated_at is later than created_at date");
   } catch (error) {
     console.log(error);
   }
 }
-getBoomTownInfo();
-
-// axios.get("http://api.github.com/orgs/boomtownroi"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/events"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/hooks"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/issues"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/members{/member}"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/public_members{/member}"),
-//   axios.get("https://api.github.com/orgs/BoomTownROI/public_members{/member}");
-//       ])
-//     then(axio.spread(url1, url2, url3, url4, url5, url6) => {
-//     console.log(url1.data.id);
-//     //   });
+verification();
